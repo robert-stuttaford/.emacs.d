f22fff1fd892618a226e282d88afca2919270765
Robert Stuttaford <robert@cognician.com>
2013-08-21 15:08:22 +0200

    show nrepl port

--- init.el
+++ init.el
@@ -62,6 +62,7 @@
  '(fci-rule-color "#393939")
  '(nrepl-host "localhost")
  '(nrepl-port "9991")
+ '(nrepl-buffer-name-show-port t)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map (quote ((20 . "#f2777a") (40 . "#f99157") (60 . "#ffcc66") (80 . "#99cc99") (100 . "#66cccc") (120 . "#6699cc") (140 . "#cc99cc") (160 . "#f2777a") (180 . "#f99157") (200 . "#ffcc66") (220 . "#99cc99") (240 . "#66cccc") (260 . "#6699cc") (280 . "#cc99cc") (300 . "#f2777a") (320 . "#f99157") (340 . "#ffcc66") (360 . "#99cc99"))))
  '(vc-annotate-very-old-color nil))
