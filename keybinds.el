d0cd0a40fbe6ca34be7b2ad2455739d9f61773b2
Robert Stuttaford <robert@cognician.com>
2015-07-19 22:14:34 +0200

    Fix keybind bug. Re-add window-resize commands.

--- keybinds.el
+++ keybinds.el
@@ -55,7 +55,7 @@
     (insert "(user/reset)")
     (cider-repl-return)))
 
-(global-set-key (kbd "C-u C-c r") 'cider-repl-hard-reset)
+(global-set-key (kbd "C-c C-c r") 'cider-repl-hard-reset)
 
 (defun cider-repl-reset ()
   (interactive)
@@ -127,3 +127,64 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-M-%") 'query-replace)
+
+;; Resize the current window
+
+(defun win-resize-top-or-bot ()
+  "Figure out if the current window is on top, bottom or in the
+middle"
+  (let* ((win-edges (window-edges))
+         (this-window-y-min (nth 1 win-edges))
+         (this-window-y-max (nth 3 win-edges))
+         (fr-height (frame-height)))
+    (cond
+     ((eq 0 this-window-y-min) "top")
+     ((eq (- fr-height 1) this-window-y-max) "bot")
+     (t "mid"))))
+
+(defun win-resize-left-or-right ()
+  "Figure out if the current window is to the left, right or in the
+middle"
+  (let* ((win-edges (window-edges))
+         (this-window-x-min (nth 0 win-edges))
+         (this-window-x-max (nth 2 win-edges))
+         (fr-width (frame-width)))
+    (cond
+     ((eq 0 this-window-x-min) "left")
+     ((eq (+ fr-width 4) this-window-x-max) "right")
+     (t "mid"))))
+
+(defun win-resize-enlarge-vert ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window -5))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window 5))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window -5))
+   (t (message "nil"))))
+
+(defun win-resize-minimize-vert ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window 5))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window -5))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window 5))
+   (t (message "nil"))))
+
+(defun win-resize-enlarge-horiz ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally -30))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally 30))
+   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally 30))))
+
+(defun win-resize-minimize-horiz ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally 30))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally -30))
+   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally -30))))
+
+(global-set-key [s-S-left] 'win-resize-enlarge-horiz)
+(global-set-key [s-S-right] 'win-resize-minimize-horiz)
+(global-set-key [s-S-up] 'win-resize-enlarge-vert)
+(global-set-key [s-S-down] 'win-resize-minimize-vert)
