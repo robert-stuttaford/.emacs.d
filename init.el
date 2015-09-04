8c4f94d06a6b21beb0ee2b97bbe467d2228fec21
Robert Stuttaford <robert@cognician.com>
2015-09-04 14:01:38 +0200

    Remove patch for starter-kit.

--- init.el
+++ init.el
@@ -49,10 +49,6 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-(require 'magit)
-(defvar magit-diff-add 'magit-diff-added)
-(defvar magit-diff-del 'magit-diff-removed)
-
 ;; Load key bindings.
 (load (concat user-emacs-directory "keybinds.el"))
 
