ac995f4a79cff37aa3a7dfb06d8b5add55cdecb7
Robert Stuttaford <robert@cognician.com>
2025-07-18 08:20:42 +0200

    see all test failures

--- custom.el
+++ custom.el
@@ -15,6 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-test-fail-fast nil)
  '(hl-todo-keyword-faces
    '(("TODO" warning bold) ("FIXME" error bold)
      ("HACK" font-lock-constant-face bold)
